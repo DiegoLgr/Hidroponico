c1c1bd588f9862b1a1d8126dbe08600d5d8b63b0
candy0709 <93722059+candy0709@users.noreply.github.com>
2021-11-04 18:51:37 +0100

    Update sensor.cpp

--- thingiverse/sensor.cpp
+++ thingiverse/sensor.cpp
@@ -1,123 +1,47 @@
-#include "sensor.h"
+#include <Arduino.h>
 #include <WiFi.h>           // WiFi control for ESP32
 #include <ThingsBoard.h>    // ThingsBoard SDK
 
-#define DHTPIN 33     // what digital pin we're connected to
-
-
-// Helper macro to calculate array size
-#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
-
-// WiFi access point
-#define WIFI_AP_NAME       "Carlajijiji"// "WIFI_AP"
-// WiFi password
-#define WIFI_PASSWORD      "candy115"// "WIFI_PASSWORD"
-
-// See https://thingsboard.io/docs/getting-started-guides/helloworld/
-// to understand how to obtain an access token
-#define TOKEN              "lGhZDVbx1y5M0cm8WM8o" // "TOKEN"
-// ThingsBoard server instance.
+#include "sensor.h"
 #define THINGSBOARD_SERVER  "demo.thingsboard.io"
-
-// Baud rate for debug serial
-#define SERIAL_DEBUG_BAUD    115200
-
-// Initialize ThingsBoard client
-WiFiClient espClient;
-// Initialize ThingsBoard instance
-ThingsBoard tb(espClient);
-// the Wifi radio's status
-int status = WL_IDLE_STATUS;
-
-// Period of sending a temperature/humidity data.
-int send_delay = 2000;
-unsigned long millis_counter;
-
-
-void InitWiFi()
-{
-  Serial.println("Connecting to AP ...");
-  // attempt to connect to WiFi network
-
-  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) 
-	{
-    delay(500);
-    Serial.print(".");
- 	 }
-  Serial.println("Connected to AP");
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  status = WiFi.status();
-  if ( status != WL_CONNECTED) {
-    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("Connected to AP");
-  }
-}
-
-// Setup an application
-void setup() {
-  // Initialize serial for debugging
-  Serial.begin(SERIAL_DEBUG_BAUD);
-  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
-  InitWiFi();
-}
-
-
-
+#define TOKEN              "lGhZDVbx1y5M0cm8WM8o" // "TOKEN"
 //-------------------------------------------------------------------------------
 //Sensor constructor
-sensor::sensor(int temperatura, int ec, int ph, int nivel_reservorio)
-{
-	m_temperatura = temperatura;
-	m_ec = ec;
-	m_ph = ph;
-	m_nivel_reservorio = nivel_reservorio;
-}
+Sensor::Sensor(int temperatura1, int ec1, int ph1, int nivel_reservorio1, ThingsBoard tb1)
+ {
+    temperatura0=temperatura1;
+    ec0=ec1;
+    ph0=ph1;
+    nivel_reservorio0=nivel_reservorio1;
+    tb0=tb1;
+  }
 
 //Sensor member function
-void set_temperatura(int temp){temperatura = temp;}
+void Sensor::set_temperatura(int temperatura){temperatura0 = temperatura;}
 
-void set_EC(int ecc){ec = ecc;}
+void Sensor::set_EC(int ec){ec0 = ec;}
 
-void set_nivel_reservorio(int nivel_reser){nivel_reservorio = nivel_reser;}
+void Sensor::set_nivel_reservorio(int nivel_reservorio){nivel_reservorio0 = nivel_reservorio;}
 
-void set_PH(int phh){ph = phh;}
+void Sensor::set_PH(int ph){ph0 = ph;}
 //-------------------------------------------------------------------------------
-
-
-void actualizar_thingsboard(int temperatura, int ec, int ph, int nivel_reservorio)
+void Sensor::actualizar_thingsboard(int temperatura, int ec, int ph, int nivel_reservorio)
 {
-  // Reconnect to WiFi, if needed
-  if (WiFi.status() != WL_CONNECTED) {
-    reconnect();
-    return;
-  }
 
 
   // Reconnect to ThingsBoard, if needed
-  if (!tb.connected()) {
+  if (!tb0.connected()) {
     // Connect to the ThingsBoard
     Serial.print("Connecting to: ");
     Serial.print(THINGSBOARD_SERVER);
     Serial.print(" with token ");
     Serial.println(TOKEN);
-    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+    if (!tb0.connect(THINGSBOARD_SERVER, TOKEN)) {
       Serial.println("Failed to connect");
       return;
     }
   }
 
-  // Check if it is a time to send DHT11 temperature and humidity
-  if(millis()-millis_counter > send_delay) {
-    Serial.println("Sending data...");
-	}
 
 if (isnan(temperatura) || isnan(ec) || isnan(ph) || isnan(nivel_reservorio)) {
       Serial.println("Failed to read from DHT sensor!");
@@ -130,12 +54,9 @@ if (isnan(temperatura) || isnan(ec) || isnan(ph) || isnan(nivel_reservorio)) {
       Serial.println(ph);
 	  Serial.print(" Nivel reservorio ");
       Serial.println(nivel_reservorio);
-
-      tb.sendTelemetryFloat("temperature", temperatura);
-      tb.sendTelemetryFloat("nivel reservorio", nivel_reservorio);
-	  tb.sendTelemetryFloat("ec", ec);
-      tb.sendTelemetryFloat("ph", ph);
+      tb0.sendTelemetryFloat("temperature", temperatura);
+      tb0.sendTelemetryFloat("nivel reservorio", nivel_reservorio);
+	  tb0.sendTelemetryFloat("ec", ec);
+      tb0.sendTelemetryFloat("ph", ph);
     }
-
-  millis_counter = millis(); //reset millis counter
 }
