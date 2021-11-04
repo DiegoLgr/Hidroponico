210cc26cda203a77878edde9c7c13e4b80abfc38
jorgePfff <93338525+jorgePfff@users.noreply.github.com>
2021-11-04 14:02:07 +0100

    Create TestNivelBuffer.h

---
+++ sensores/TestNivelBuffer.h
@@ -0,0 +1,15 @@
+#ifndef TESTNIVELBUFFER_H
+#define TESTNIVELBUFFER_H
+
+class TestNivelBuffer
+{
+private:
+    int pin;
+    
+    int read():
+public:
+    TestNivelBuffer(int pinNivelBuffer);
+     
+};
+
+#endif
