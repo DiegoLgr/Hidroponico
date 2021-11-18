a9a565644b117b7651af8fd13d760ff9b0ffb402
candy0709 <93722059+candy0709@users.noreply.github.com>
2021-11-18 15:49:05 +0100

    Update sensor.h

--- thingiverse/sensor.h
+++ thingiverse/sensor.h
@@ -4,20 +4,20 @@
 class Sensor
 {
  private:
-    int temperatura0;
-    int ec0;
+    float temperatura0;
+    float ec0;
     int ph0;
-    int nivel_reservorio0;
+    float nivel_reservorio0;
     ThingsBoard tb0;
 
 public:
-    void set_temperatura(int temperatura);
-    void set_EC(int ec);
-    void set_nivel_reservorio(int nivel_reservorio);
+    void set_temperatura(float temperatura);
+    void set_EC(float ec);
+    void set_nivel_reservorio(float nivel_reservorio);
     void set_PH(int ph);
-    void actualizar_thingsboard(int temperatura, int ec, int ph, int nivel_reservorio);
+    void actualizar_thingsboard(float temperatura, float ec, int ph, float nivel_reservorio);
     Sensor()=default;
- Sensor(int temperatura1, int ec1, int ph1, int nivel_reservorio1, ThingsBoard tb1);
+ Sensor(float temperatura1, float ec1, int ph1, float nivel_reservorio1, ThingsBoard tb1);
  
 };
 #endif
