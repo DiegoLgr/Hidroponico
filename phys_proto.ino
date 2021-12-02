57fa214506dfe3c5afe0438b841b2586d1b0ac5a
jorgePfff <93338525+jorgePfff@users.noreply.github.com>
2021-12-02 18:44:29 +0100

    Add files via upload

--- phys_proto.ino
+++ phys_proto.ino
@@ -21,23 +21,25 @@
 #define ALTO true
 #define BAJO false
 
+int ronda =0;
 int i = 0;
-
+int nivelv;
+float phv, ecv, turbidezv;
 rgb_lcd lcd;
 
-Sensor_nivel sensor_nivelBufferAlto(27);
-Sensor_nivel sensor_nivelBufferBajo(27);
-Sensor_nivel sensor_nivelReservorioAlto(27);
-Sensor_nivel sensor_nivelReservorioBajo(27);
+Sensor_nivel sensor_nivelBufferAlto(32);
+Sensor_nivel sensor_nivelBufferBajo(35);
+Sensor_nivel sensor_nivelReservorioAlto(25);
+Sensor_nivel sensor_nivelReservorioBajo(26);
 
-Sensor_turbidez sensor_turbidez(11);
+Sensor_turbidez sensor_turbidez(34);
 
-Sensor_PH sensor_ph(9);
+Sensor_PH sensor_ph(12);
 
-Sensor_EC sensor_ec(10);
+Sensor_EC sensor_ec(33);
 
-Cubeta cubeta { Bomba{13} };
-Buffer bbuffer { Bomba{13}, sensor_nivelBufferBajo, sensor_nivelBufferAlto};
+Cubeta cubeta { Bomba{14} };
+Buffer bbuffer { Bomba{27}, sensor_nivelBufferBajo, sensor_nivelBufferAlto};
 Reservorio reservorio {cubeta, sensor_nivelReservorioBajo, sensor_nivelReservorioAlto, Notificador{}};
 
 String msgScroll = "PH=0,EC=0,T=0C,NR=0";
@@ -115,42 +117,65 @@ void scrollText(int row, String message, int delayTime, int lcdColumns) {
 SensorTB nuevo_sensor(tb);  
 
 void setup(){
-  pinMode(27, INPUT);
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
+  pinMode(25, INPUT);
+  pinMode(26, INPUT);
+  pinMode(35, INPUT);
+  pinMode(12, INPUT);
+  pinMode(35, INPUT);
+  pinMode(14, OUTPUT);
+  pinMode(27, OUTPUT);
+  
   Serial.begin(115200);
   lcd.begin(16, 2);
       // Initialize serial for debugging
-  Serial.begin(SERIAL_DEBUG_BAUD);
   WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
   InitWiFi();
   }
+
 
 void loop(){
   if (WiFi.status() != WL_CONNECTED) {
     reconnect();
   return;
   }
-  
-
-  nuevo_sensor.actualizar_thingsboard(sensor_turbidez.read(), sensor_ec.read(), sensor_ph.read(), sensor_nivelReservorioBajo.read());
+  ronda = (ronda+1)%3;
+  if (ronda ==0){
+    phv = sensor_ph.read();
+    adcAttachPin(13);
+  }
+  if (ronda ==1){
+    ecv = sensor_ec.read();
+    adcAttachPin(34);
+  }
+  if (ronda ==2){
+    turbidezv = sensor_turbidez.read();
+    adcAttachPin(12);
+  }
+  nivelv = sensor_nivelReservorioBajo.read();
+  Serial.print("sensor buffer bajo");
+  Serial.println(sensor_nivelBufferBajo.read());
+  nuevo_sensor.actualizar_thingsboard(turbidezv, ecv, phv, nivelv);
   
   msgScroll = "PH="; 
-  msgScroll = msgScroll + sensor_ph.read();
+  msgScroll = msgScroll + String(phv);
   msgScroll = msgScroll + ",EC=" ;
-  msgScroll = msgScroll + sensor_ec.read();
+  msgScroll = msgScroll + String(ecv);
   msgScroll = msgScroll + ",T=" ;
-  msgScroll = msgScroll + sensor_turbidez.read();
+  msgScroll = msgScroll + String(turbidezv);
   msgScroll = msgScroll + ",NR=";
-  msgScroll = msgScroll + sensor_nivelReservorioBajo.read();
+  msgScroll = msgScroll + String(nivelv);
         
   lcd.setCursor(0, 0);
 
       //incluir condiciones de Alarma
   if (true)
     lcd.print("ALARMA");
-  scrollText(1,msgScroll,250,16);
-       
-  if (i ==20)
+ // scrollText(1,msgScroll,250,16);
+  
     bbuffer.tick(10);
     reservorio.tick(10);
-    delay(200);
+    delay(500);
   }
+  
